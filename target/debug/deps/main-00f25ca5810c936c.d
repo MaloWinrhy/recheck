74e95a854aea9b9afcca0aeaa75847bbc4ec7e5f
MaloWinrhy <malo@arkada.fr>
2025-08-05 16:36:46 +0200

    Update dependencies and adjust project structure
    - Changed the Rust edition from 2024 to 2021 in Cargo.toml.
    - Added several dependencies: clap, blake3, walkdir, normalize-line-endings, flate2, tar, zip, and chrono.
    - Removed various generated files and directories from the target/debug folder, including binary and metadata files.
    - Deleted the .fingerprint and incremental build files related to previous builds.
    - Added a .gitignore file to exclude target files, Cargo.lock, IDE files, OS generated files, temporary files, and environment files.

--- target/debug/deps/main-00f25ca5810c936c.d
+++
@@ -1,5 +0,0 @@
-D:\dev\learning\recheck\target\debug\deps\main-00f25ca5810c936c.d: src\bin\main.rs
-
-D:\dev\learning\recheck\target\debug\deps\libmain-00f25ca5810c936c.rmeta: src\bin\main.rs
-
-src\bin\main.rs:
--- target/debug/deps/recheck-7f49af154749682d.d
+++
@@ -1,5 +0,0 @@
-D:\dev\learning\recheck\target\debug\deps\recheck-7f49af154749682d.d: src\lib.rs
-
-D:\dev\learning\recheck\target\debug\deps\librecheck-7f49af154749682d.rmeta: src\lib.rs
-
-src\lib.rs:
